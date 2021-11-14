b5ca161312c84fecacec4db160f3872f7f6c0596
Marcos Vinicius <gbarbosa@fatorseguradora.com.br>
2021-11-14 11:24:50 -0300

    Rotas Configuradas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Http\Controllers\AtividadesController;
+use App\Http\Controllers\BoletosController;
+use App\Http\Controllers\ConfigSetoresController;
+use App\Http\Controllers\ConfigTarefasController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\EmpresasController;
+use App\Http\Controllers\RelatoriosController;
+use App\Http\Controllers\SetoresController;
+use App\Http\Controllers\TarefasController;
+use App\Models\ConfigAtividades;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +27,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('dashboard', DashboardController::class);
+Route::resource('tarefas', TarefasController::class);
+Route::resource('setores', SetoresController::class);
+Route::resource('empresas', EmpresasController::class);
+Route::resource('boletos', BoletosController::class);
+Route::resource('atividades', AtividadesController::class);
+Route::resource('relatorios', RelatoriosController::class);
+
+Route::resource('config-tarefas', ConfigTarefasController::class);
+Route::resource('config-atividades', ConfigAtividades::class);
+Route::resource('config-setores', ConfigSetoresController::class);
