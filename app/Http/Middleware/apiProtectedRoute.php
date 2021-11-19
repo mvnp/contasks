4daa676450c7d0377ea486815ee41087a4e2cfc4
Marcos Vinicius <gbarbosa@fatorseguradora.com.br>
2021-11-19 12:43:09 -0300

    JWT Setup

--- app/Http/Middleware/apiProtectedRoute.php
+++ app/Http/Middleware/apiProtectedRoute.php
@@ -15,7 +15,7 @@ class apiProtectedRoute extends BaseMiddleware
             $user = JWTAuth::parseToken()->authenticate();
         } catch (\Exception $e) {
             if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
-                return response()->json(['status' => 'Token ios Invalid']);
+                return response()->json(['status' => 'Token is Invalid']);
             } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
                 return response()->json(['status' => 'Token is Expired']);
             } else {
