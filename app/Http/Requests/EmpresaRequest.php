c54d0722e625c33ad7a08d570c33a97dc9c7a7ff
Marcos Vinicius <gbarbosa@fatorseguradora.com.br>
2021-11-21 13:46:25 -0300

    Added Portuguese Language

--- app/Http/Requests/EmpresaRequest.php
+++ app/Http/Requests/EmpresaRequest.php
@@ -44,87 +44,86 @@ class EmpresaRequest extends FormRequest
     {
         return [
             'fantasia' => [
-                'required' => 'O campo fantasia é obrigatorio!.',
-                'min' => 'O campo fantasia deve ter o mínimo de 6 caracteres.',
-                'max' => 'O campo fantasia deve ter o mínimo de 255 caracteres.'
+                'required' => 'O campo :attribute é obrigatorio!.',
+                'min' => 'O campo :attribute deve ter o mínimo de 6 caracteres.',
+                'max' => 'O campo :attribute deve ter o mínimo de 255 caracteres.'
             ],
 
-            'razao' => [
-                'required' => 'O campo razao é obrigatorio!.',
-                'min' => 'O campo razao deve ter o mínimo de 6 caracteres.',
-                'max' => 'O campo razao deve ter o mínimo de 255 caracteres.'
-            ],
+            // 'razao' => [
+            //     'required' => 'O campo razao é obrigatorio!.',
+            //     'min' => 'O campo razao deve ter o mínimo de 6 caracteres.',
+            //     'max' => 'O campo razao deve ter o mínimo de 255 caracteres.'
+            // ],
 
-            'cnpj' => [
-                'required' => 'O campo cnpj é obrigatorio!.',
-                'min' => 'O campo cnpj deve ter o mínimo de 14 caracteres.',
-                'max' => 'O campo cnpj deve ter o mínimo de 14 caracteres.',
-                'numeric' => 'O campo cnpj deve ter apenas numeros.'
-            ],
+            // 'cnpj' => [
+            //     'required' => 'O campo cnpj é obrigatorio!.',
+            //     'min' => 'O campo cnpj deve ter o mínimo de 14 caracteres.',
+            //     'max' => 'O campo cnpj deve ter o mínimo de 14 caracteres.',
+            //     'numeric' => 'O campo cnpj deve ter apenas numeros.'
+            // ],
 
-            'atividade' => [
-                'required' => 'O campo atividade é obrigatorio!.',
-                'min' => 'O campo atividade deve ter o mínimo de 6 caracteres.',
-                'max' => 'O campo atividade deve ter o mínimo de 255 caracteres.'
-            ],
+            // 'atividade' => [
+            //     'required' => 'O campo atividade é obrigatorio!.',
+            //     'min' => 'O campo atividade deve ter o mínimo de 6 caracteres.',
+            //     'max' => 'O campo atividade deve ter o mínimo de 255 caracteres.'
+            // ],
 
-            'rua' => [
-                'required' => 'O campo rua é obrigatorio!.',
-                'min' => 'O campo rua deve ter o mínimo de 6 caracteres.',
-                'max' => 'O campo rua deve ter o mínimo de 255 caracteres.'
-            ],
+            // 'rua' => [
+            //     'required' => 'O campo rua é obrigatorio!.',
+            //     'min' => 'O campo rua deve ter o mínimo de 6 caracteres.',
+            //     'max' => 'O campo rua deve ter o mínimo de 255 caracteres.'
+            // ],
 
-            'numero' => [
-                'required' => 'O campo numero é obrigatorio!.',
-                'min' => 'O campo numero deve ter o mínimo de 2 caracteres.',
-                'max' => 'O campo numero deve ter o mínimo de 15 caracteres.'
-            ],
+            // 'numero' => [
+            //     'required' => 'O campo numero é obrigatorio!.',
+            //     'min' => 'O campo numero deve ter o mínimo de 2 caracteres.',
+            //     'max' => 'O campo numero deve ter o mínimo de 15 caracteres.'
+            // ],
 
-            'completmento' => [
-                'required' => 'O campo completmento é obrigatorio!.',
-                'min' => 'O campo completmento deve ter o mínimo de 6 caracteres.',
-                'max' => 'O campo completmento deve ter o mínimo de 255 caracteres.'
-            ],
+            // 'completmento' => [
+            //     'required' => 'O campo completmento é obrigatorio!.',
+            //     'min' => 'O campo completmento deve ter o mínimo de 6 caracteres.',
+            //     'max' => 'O campo completmento deve ter o mínimo de 255 caracteres.'
+            // ],
 
-            'bairro' => [
-                'required' => 'O campo bairro é obrigatorio!.',
-                'min' => 'O campo bairro deve ter o mínimo de 6 caracteres.',
-                'max' => 'O campo bairro deve ter o mínimo de 255 caracteres.'
-            ],
+            // 'bairro' => [
+            //     'required' => 'O campo bairro é obrigatorio!.',
+            //     'min' => 'O campo bairro deve ter o mínimo de 6 caracteres.',
+            //     'max' => 'O campo bairro deve ter o mínimo de 255 caracteres.'
+            // ],
 
-            'cep' => [
-                'required' => 'O campo cep é obrigatorio!.',
-                'min' => 'O campo cep deve ter o mínimo de 8 caracteres.',
-                'max' => 'O campo cep deve ter o mínimo de 8 caracteres.',
-                'numeric' => 'O campo cep deve ter apenas numeros.'
-            ],
+            // 'cep' => [
+            //     'required' => 'O campo cep é obrigatorio!.',
+            //     'min' => 'O campo cep deve ter o mínimo de 8 caracteres.',
+            //     'max' => 'O campo cep deve ter o mínimo de 8 caracteres.',
+            //     'numeric' => 'O campo cep deve ter apenas numeros.'
+            // ],
 
-            'cidade' => [
-                'required' => 'O campo cidade é obrigatorio!.',
-                'min' => 'O campo cidade deve ter o mínimo de 6 caracteres.',
-                'max' => 'O campo cidade deve ter o mínimo de 255 caracteres.',
-            ],
+            // 'cidade' => [
+            //     'required' => 'O campo cidade é obrigatorio!.',
+            //     'min' => 'O campo cidade deve ter o mínimo de 6 caracteres.',
+            //     'max' => 'O campo cidade deve ter o mínimo de 255 caracteres.',
+            // ],
 
-            'estado' => [
-                'required' => 'O campo estado é obrigatorio!.',
-                'min' => 'O campo estado deve ter o mínimo de 6 caracteres.',
-                'max' => 'O campo estado deve ter o mínimo de 255 caracteres.',
-            ],
+            // 'estado' => [
+            //     'required' => 'O campo estado é obrigatorio!.',
+            //     'min' => 'O campo estado deve ter o mínimo de 6 caracteres.',
+            //     'max' => 'O campo estado deve ter o mínimo de 255 caracteres.',
+            // ],
 
-            'telefonePrincipal' => [
-                'required' => 'O campo telefonePrincipal é obrigatorio!.',
-                'min' => 'O campo telefonePrincipal deve ter o mínimo de 8 caracteres.',
-                'max' => 'O campo telefonePrincipal deve ter o mínimo de 8 caracteres.',
-                'numeric' => 'O campo telefonePrincipal deve ter apenas numeros.'
-            ],
+            // 'telefonePrincipal' => [
+            //     'required' => 'O campo telefonePrincipal é obrigatorio!.',
+            //     'min' => 'O campo telefonePrincipal deve ter o mínimo de 8 caracteres.',
+            //     'max' => 'O campo telefonePrincipal deve ter o mínimo de 8 caracteres.',
+            //     'numeric' => 'O campo telefonePrincipal deve ter apenas numeros.'
+            // ],
 
-            'telefoneSecundario' => [
-                'required' => 'O campo telefoneSecundario é obrigatorio!.',
-                'min' => 'O campo telefoneSecundario deve ter o mínimo de 8 caracteres.',
-                'max' => 'O campo telefoneSecundario deve ter o mínimo de 8 caracteres.',
-                'numeric' => 'O campo telefoneSecundario deve ter apenas numeros.'
-            ],
-
+            // 'telefoneSecundario' => [
+            //     'required' => 'O campo telefoneSecundario é obrigatorio!.',
+            //     'min' => 'O campo telefoneSecundario deve ter o mínimo de 8 caracteres.',
+            //     'max' => 'O campo telefoneSecundario deve ter o mínimo de 8 caracteres.',
+            //     'numeric' => 'O campo telefoneSecundario deve ter apenas numeros.'
+            // ],
         ];
     }
 }
