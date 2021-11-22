22155c33d0ecc6935bff2cf947ecfe9470726640
Sirleii Cristina <programadora01@gmail.com>
2021-11-22 11:17:27 -0300

    commit

--- app/Http/Requests/EmpresaRequest.php
+++ app/Http/Requests/EmpresaRequest.php
@@ -26,17 +26,21 @@ class EmpresaRequest extends FormRequest
         return [
             'fantasia' =>               'required|min:6|max:255',
             'razao' =>                  'required|min:6|max:255',
-            'cnpj' =>                   'required|digits:14|numeric|unique:empresas',
+            'cnae' =>                   '',
+            'inscricaoMunicipal' =>     '',
+            'inscricaoEstadual' =>      '',
+            'cnpj' =>                   'required|digits:14|numeric|unique:empresas|cnpj',
             'atividade' =>              'required|min:6',
-            'rua' =>                    'required|min:6|max:255',
+            'rua' =>                    'required|min:4|max:255',
             'numero' =>                 'required|min:2|max:15',
-            'complemento' =>           '',
-            'bairro' =>                 'required|min:6|max:255',
+            'complemento' =>            '',
+            'bairro' =>                 'required|min:4|max:255',
             'cep' =>                    'required|digits:8',
-            'cidade' =>                 'required|min:6|max:255',
+            'cidade' =>                 'required|min:4|max:255',
             'estado' =>                 'required|min:2|max:255',
             'telefonePrincipal' =>      'required|min:10|max:11',
             'telefoneSecundario' =>     'required|min:10|max:11'
+
         ];
     }
 }
