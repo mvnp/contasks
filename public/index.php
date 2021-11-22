5b67ad28d27581a970a0ca5f3dcaead2b2de26c4
Marcos Vinicius <gbarbosa@fatorseguradora.com.br>
2021-11-22 20:39:23 -0300

    PHP Errors Show

--- public/index.php
+++ public/index.php
@@ -1,11 +1,5 @@
 <?php
 
-echo "<h1>hello</h1>";
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
