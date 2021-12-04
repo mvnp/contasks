523375869572cff02f3abc70cd05abf24bf67646
Sirleii Cristina <programadora01@gmail.com>
2021-12-04 20:38:11 -0300

    updates

--- app/Services/BoletosService.php
+++ app/Services/BoletosService.php
@@ -32,7 +32,7 @@ class BoletosService
 
     public function gerarBoleto($idDebito)
     {
-        if (FinanceiroReceber::with('boleto')->find($idDebito)->boleto_id) {
+        if (Boletos::with('boleto')->find($idDebito)->boleto_id) {
             return true;
         }
         return false;
