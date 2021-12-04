f6af2dd32fcc5588d8500f4f412f86d04245979b
Sirleii Cristina <programadora01@gmail.com>
2021-12-04 20:14:29 -0300

    updates

--- app/Repositories/BoletosRepository.php
+++ app/Repositories/BoletosRepository.php
@@ -58,7 +58,7 @@ class BoletosRepository
     public function updatePdfBoleto($idBoleto, $filename)
     {
         $model = Boletos::find($idBoleto);
-        $model->boleto_arquivo = $filenam;
+        $model->boleto_arquivo = $filename;
         $saved = $model->save();
 
         if (!$saved) {
