d3b2d56bfd4983aa0ad76936125e43b27ecb7bef
Sirleii Cristina <programadora01@gmail.com>
2021-12-06 18:42:20 -0300

    updates

--- app/Http/Controllers/BoletosController.php
+++ app/Http/Controllers/BoletosController.php
@@ -60,7 +60,7 @@ class BoletosController extends Controller
         $boletosService = new BoletosService;
         $geradorBoleto = $boletosService->gerarBoleto($id);
 
-        var_dump($geradorBoleto);
+        dd($geradorBoleto);
 
         return response()->json([
             'data' => $geradorBoleto
