1a6af08ef6783d86c24d53ce3aad08a7fe1f1d8c
Sirleii Cristina <programadora01@gmail.com>
2021-12-06 11:00:29 -0300

    updates

--- app/Http/Controllers/BoletosController.php
+++ app/Http/Controllers/BoletosController.php
@@ -73,7 +73,7 @@ class BoletosController extends Controller
 
         $result = match ($geradorBoleto) {
             $geradorBoleto >= true => response()->json(['error' => 'Boleto já existe.']),
-            $geradorBoleto >= $id => response()->json(['data' => 'Boleto foi criado com sucesso.']),
+            $geradorBoleto >= $boleto => response()->json(['data' => 'Boleto foi criado com sucesso.']),
             default => throw new \Exception('Não foi possível criar boleto'),
         };
 
