981937e4060fb0679c1813fcc77e4eb010dd679e
Marcos Vinicius <mvnpereira@gmail.com>
2021-12-07 06:29:22 -0300

    Updates

--- app/Services/BoletosService.php
+++ app/Services/BoletosService.php
@@ -36,7 +36,7 @@ class BoletosService
         $ArrayInfoDebito = $boletosRepository->getAll($idDebito);
         $boleto = $this->getBoleto($ArrayInfoDebito);
 
-        // dd($boleto);
+        dd($boleto);
 
         try {
             // MOCK
